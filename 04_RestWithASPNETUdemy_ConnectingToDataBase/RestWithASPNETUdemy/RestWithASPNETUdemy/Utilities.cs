c7840be7d5397549ffa8d1a27c4c8ad84e856b37
MatheusFCardoso <matheusfrancacardoso@gmail.com>
2024-01-02 14:23:51 -0300

    Connecting to Database

---
+++ 04_RestWithASPNETUdemy_ConnectingToDataBase/RestWithASPNETUdemy/RestWithASPNETUdemy/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    public class Utilities
+    {
+        public static bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+
+        public static decimal ConvertToDecimal (string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+    }
+}
