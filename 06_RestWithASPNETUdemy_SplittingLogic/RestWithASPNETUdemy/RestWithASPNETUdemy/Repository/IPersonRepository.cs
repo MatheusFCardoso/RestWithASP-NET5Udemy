2baeafcfee22d20fd4ff0adac147df9e459fa51f
MatheusFCardoso <matheusfrancacardoso@gmail.com>
2024-01-03 18:30:37 -0300

    Splitting Logic

---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+        bool Exists(long id);
+
+    }
+}
