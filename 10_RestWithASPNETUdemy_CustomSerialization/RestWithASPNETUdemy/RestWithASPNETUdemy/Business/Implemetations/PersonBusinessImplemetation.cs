d501f9a2dba8e61996ad74d85c3be84cfd3c5db6
MatheusFCardoso <matheusfrancacardoso@gmail.com>
2024-01-26 14:54:48 -0300

    Custom Serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implemetations/PersonBusinessImplemetation.cs
@@ -0,0 +1,50 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Implementations;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implemetations
+{
+    public class PersonBusinessImplemetation : IPersonBusiness
+    {
+
+        private readonly IRepository<Person> _repository;
+        private readonly PersonConverter _converter;
+        public PersonBusinessImplemetation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        public List<PersonVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public PersonVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+    }
+}
