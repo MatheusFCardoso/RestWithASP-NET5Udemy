93f2e04eaf4a55f906a7fa2f88248e8e6fe3e392
MatheusFCardoso <matheusfrancacardoso@gmail.com>
2024-01-26 14:39:40 -0300

    Working With Value Objects

---
+++ 09_RestWithASPNETUdemy_WorkingWithValueObjects/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Generic/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Base;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindById(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+
+    }
+}
