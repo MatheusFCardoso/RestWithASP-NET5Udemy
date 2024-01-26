756b4bb712d72f59a863ce454b9488e5a921f51f
MatheusFCardoso <matheusfrancacardoso@gmail.com>
2024-01-26 15:27:30 -0300

    Working with Content Negotiation

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implemetations/BookBusinessImplemetation.cs
@@ -0,0 +1,49 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Implementations;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implemetations
+{
+    public class BookBusinessImplemetation : IBookBusiness
+    {
+        private IRepository<Book> _repository;
+        private BookConverter _converter;
+
+        public BookBusinessImplemetation(IRepository<Book> repository)
+        {
+            _repository = repository;
+            _converter = new BookConverter();
+        }
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<BookVO> Index()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public BookVO Show(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        public BookVO Store(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        public BookVO Update(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Update(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+    }
+}
---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BookController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IEnumerable<BookVO> Index()
+        {
+            return _bookBusiness.Index();
+        }
+        [HttpGet("{id}")]
+        public BookVO Show(long id)
+        {
+            return _bookBusiness.Show(id);
+        }
+        [HttpPost]
+        public BookVO Store(BookVO book)
+        {
+            return _bookBusiness.Store(book);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent(); 
+        }
+        [HttpPut]
+        public BookVO Update(BookVO book)
+        {
+           return  _bookBusiness.Update(book);
+        } 
+    }
+}
